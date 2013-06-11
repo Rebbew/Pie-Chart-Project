3052002ed380a3436c437fe32bfaf70cb0b2b59f
rebbew <mjbookworm93@gmail.com>
2013-06-11 01:11:37 -0400

    7 years and it's finally here!!!!

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 	Input Excel File: <input type="text" name="exfile"><br>
     <?php
 error_reporting(E_ALL ^ E_NOTICE);
-require_once("../lib/phpchartdir.php");
-require_once 'excel_reader2.php';
+require_once("../ChartDirector/lib/phpchartdir.php");
+require_once ("../Excel_reader2/excel_reader2.php");
 $file = new Spreadsheet_Excel_Reader("exfile");
 # The data for the pie chart
 $data = array();
@@ -22,19 +22,21 @@ $data[] = $file.getCol(1);
 # The labels for the pie chart
 $labels = array();
 $labels = $file.getCol(1)
+
 # Create a PieChart object of size 360 x 300 pixels
 $c = new PieChart(360, 300);
 
 # Set the center of the pie at (180, 140) and the radius to 100 pixels
-$c->setPieSize(180, 140, 100);
+$c.setPieSize(180, 140, 100);
 
 # Set the pie data and the pie labels
-$c->setData($data, $labels);
+$c.setData($data, $labels);
 
 # Output the chart
 header("Content-type: image/png");
-print($c->makeChart2(PNG));
-?>
+print($c.makeChart2(PNG));
+
+    ?>
 	
 </body>
 </html>
