8037ed44b89fe4888627bff8dd58de500e51e167
Rebbew <mjbookworm93@gmail.com>
2013-06-11 13:55:42 -0400

    added uploads

--- index.php
+++ index.php
@@ -1,42 +1,42 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Pie Chart converter</title>
-    <meta content="Matthew Webber" name="author">
-    <meta content="Pie Chart Programming Project" name="description">
-    <link rel="stylesheet" type="text/css" href="../styles/main.css">
-    <h1> Spreadsheet to Pie Chart Converter </h1>
-</head>
+<?php
+    require_once('header.php');
+?>
 <body>
-	Input Excel File: <input type="text" name="exfile"><br>
+	<form enctype="multipart/form-data" action="uploader.php" method="POST">
+        <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+        Choose a file to upload: <input name="uploadedfile" type="file" /><br />
+        <input type="submit" value="Upload File" />
+    </form>
     <?php
-error_reporting(E_ALL ^ E_NOTICE);
-require_once("../ChartDirector/lib/phpchartdir.php");
-require_once ("../Excel_reader2/excel_reader2.php");
-$file = new Spreadsheet_Excel_Reader("exfile");
-# The data for the pie chart
-$data = array();
-$data[] = $file.getCol(1);
+        error_reporting(E_ALL ^ E_NOTICE);
+        require_once("../ChartDirector/lib/phpchartdir.php");
+        require_once ("../Excel_reader2/excel_reader2.php");
+        $file = new Spreadsheet_Excel_Reader("exfile");
+        # The data for the pie chart
+        $data = array();
+        $data[] = $file.getCol(0);
 
-# The labels for the pie chart
-$labels = array();
-$labels = $file.getCol(1)
+        # The labels for the pie chart
+        $labels = array();
+        $labels[] = $file.getCol(1)
 
-# Create a PieChart object of size 360 x 300 pixels
-$c = new PieChart(360, 300);
+        # Create a PieChart object of size 360 x 300 pixels
+        $c = new PieChart(360, 300);
 
-# Set the center of the pie at (180, 140) and the radius to 100 pixels
-$c.setPieSize(180, 140, 100);
+        # Set the center of the pie at (180, 140) and the radius to 100 pixels
+        $c.setPieSize(180, 140, 100);
 
-# Set the pie data and the pie labels
-$c.setData($data, $labels);
+        # Set the pie data and the pie labels
+        $c.setData($data, $labels);
 
-# Output the chart
-header("Content-type: image/png");
-print($c.makeChart2(PNG));
-
+        # Output the chart
+        header("Content-type: image/png");
+        print($c.makeChart2(PNG));
     ?>
-	
 </body>
+    <?php
+        require_once('footer.php');
+    ?>
 </html>
